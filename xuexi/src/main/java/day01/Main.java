402f1a33af6f6dbc954b15b323ef595cd66580ea
Lenovo <2501380009@qq.com>
2026-06-15 17:02:17 +0800

    更新

---
+++ xuexi/src/main/java/day01/Main.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        int a=10;
+        byte b=20;
+        b=(byte)a;
+        System.out.println(b);
+    }
+}
